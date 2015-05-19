c0cf4bdd7d86e66c6812b8a86a51025a0824cf7c
wonjinwonjin <wonjin911@gmail.com>
2015-05-20 00:12:34 +0900

    sorting, random matching completed

--- smp_matching.py
+++ smp_matching.py
@@ -1,5 +1,6 @@
 import os, sys
-
+import operator
+import random 
 
 class Person():
     
@@ -21,6 +22,7 @@ class Person():
 
         ''' score list of other Person '''
         self.score_dic = {}
+        self.score_sum = 0.0
 
     def set_sleep_time(self, n):
         self.sleep_time = n
@@ -63,11 +65,21 @@ def insert_person_information(p, sleep_time, getup_time, smoke_flag, rm_smoke_fl
 
 def score_compute_all(person_dic, person_num):
     
+    score_sum_dic = {}
+
     for i in range(1, person_num):
         for j in range(i+1, person_num):
             score = score_compute(person_dic[i], person_dic[j])
             person_dic[i].score_dic_update(j, score)
             person_dic[j].score_dic_update(i, score)
+
+    for i in range(1, person_num):
+        for value in person_dic[i].score_dic.values():
+            person_dic[i].score_sum += value;
+        score_sum_dic[i] = person_dic[i].score_sum
+
+    # sort score_sum_dic
+    return sorted(score_sum_dic.items(), key=operator.itemgetter(1), reverse=True)
 
 def score_compute(p1, p2):
 
@@ -147,12 +159,14 @@ def score_clean(p1, p2):
         score = 0
     return score
 
-def smp_matching(person_dic):
+def smp_matching(person_dic, score_sum_list):
     
     score_sum = 0
     result_dic = {}
+    
+    for item in score_sum_list:
+        n = item[0]
 
-    for n in person_dic:
         if n not in result_dic.values():
             target_n = max(person_dic[n].score_dic.iterkeys(), key=lambda k: person_dic[n].score_dic[k])
             result_dic[n] = target_n
@@ -171,10 +185,17 @@ def random_matching(person_dic):
 
     score_sum = 0
     result_dic = {}
+    choice_list = []
     for n in person_dic:
-        if n%2 == 1:
-            score_sum = score_sum + person_dic[n].score_dic[n+1]
-            result_dic[n] = n+1
+        choice_list.append(n)
+
+    while(choice_list):
+        n = random.choice(choice_list)
+        choice_list.remove(n)
+        target_n = random.choice(choice_list)
+        score_sum = score_sum + person_dic[n].score_dic[target_n]
+        result_dic[n] = target_n
+        choice_list.remove(target_n)
 
     return score_sum, result_dic
 
@@ -195,23 +216,22 @@ def insert_data(path):
     return person_dic
 
 def main():
-    
+ 
+    score_sum_list = []
     ''' Step0. get data from csv '''
     person_dic = insert_data("male.csv")
 
     ''' Step1. score computating '''
-    score_compute_all(person_dic, len(person_dic)+1)
+    score_sum_list = score_compute_all(person_dic, len(person_dic)+1)
 
     ''' Step2. SMP Matching '''
+
     rd_sum, rd_dic = random_matching(person_dic)
-    print rd_dic
     print "random sum : %d\n" % rd_sum
 
-    score_sum, result_dic = smp_matching(person_dic)
+    score_sum, result_dic = smp_matching(person_dic, score_sum_list)
     print result_dic
     print "sum : %d\n" % score_sum
-
-
 if __name__ == '__main__':
     main()
 
